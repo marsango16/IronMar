7fa148589e35fce6cea0d5a13c3e3437864607ae
Mar Sánchez <marsango.mu@gmail.com>
2023-04-04 18:01:50 +0200

    SQL-Join_Two_Tables

---
+++ Mar Labs/WEEK 3/SQL-Join_Two_Tables.sql
@@ -0,0 +1,55 @@
+USE sakila;
+
+-- 1 Query 
+-- Which actor has appeared in the most films?
+SELECT CONCAT(actor.First_name, ' ', actor.Last_name) AS Full_name, COUNT(film.film_id) AS total_films
+FROM actor
+INNER JOIN film_actor ON actor.actor_id = film_actor.actor_id
+INNER JOIN film ON film_actor.film_id = film.film_id
+GROUP BY actor.actor_id, actor.First_name, actor.Last_name
+ORDER BY total_films DESC
+LIMIT 1;
+
+-- 2 Query 
+-- Most active customer (the customer that has rented the most number of films)
+SELECT CONCAT(customer.first_name, ' ', customer.last_name) AS Full_name, COUNT(rental.rental_id) AS total_rentals
+FROM customer
+INNER JOIN rental ON customer.customer_id = rental.customer_id
+INNER JOIN inventory ON rental.inventory_id = inventory.inventory_id
+INNER JOIN film ON inventory.film_id = film.film_id
+GROUP BY customer.customer_id, customer.first_name, customer.last_name
+ORDER BY total_rentals DESC
+LIMIT 1;
+
+-- 3 Query 
+-- List number of films per category.
+SELECT category.name AS Category, COUNT(*) AS Number_of_films
+FROM film_category
+INNER JOIN category ON film_category.category_id = category.category_id
+GROUP BY category.name;
+
+-- 4 Query
+-- Display the first and last names, as well as the address, of each staff member.
+SELECT CONCAT(staff.first_name, ' ', staff.last_name) AS Full_name, address.address AS Address
+FROM staff
+INNER JOIN address ON address.address_id = staff.address_id;
+
+-- 5 Query
+-- Get films titles where the film language is either English or italian, and whose titles starts with letter "M" , sorted by title descending.
+SELECT film.title AS Film, language.name AS Language
+FROM film
+INNER JOIN language ON film.language_id = language.language_id
+WHERE language.name IN('English');
+
+-- 6 Query
+-- Display the total amount rung up by each staff member in August of 2005.
+select a.first_name, a.last_name, sum(b.amount)
+from staff as a 
+inner join payment as b
+using (staff_id)
+where payment_date > "2005-08-01" AND payment_date < "2005-08-31"
+group by staff_id
+order by staff_id desc;
+
+-- 7 Query
+-- List each film and the number of actors who are listed for that film.
--- Mar Labs/WEEK 3/SQL_intro.sql
+++ Mar Labs/WEEK 3/SQL_intro.sql
@@ -94,5 +94,33 @@ WHERE account_id = 793
 ORDER BY date DESC
 LIMIT 10;
 
+-- Query 13
+-- In the client table, of all districts with a district_id lower than 10, how many clients are from each district_id? Show the results sorted by the district_id in ascending order. --
+SELECT district_id, COUNT(*) AS client
+FROM client
+WHERE district_id < 10
+GROUP BY district_id
+ORDER BY district_id ASC;
+
+-- Query 14
+-- In the card table, how many cards exist for each type? Rank the result starting with the most frequent type. --
+SELECT type, COUNT(*) AS card
+FROM card
+GROUP BY type
+ORDER BY card DESC;
+
+-- Query 15
+-- Using the loan table, print the top 10 account_ids based on the sum of all of their loan amounts. --
+SELECT account_id, SUM(amount) AS total_loan_amount
+FROM loan
+GROUP BY account_id
+ORDER BY total_loan_amount DESC
+LIMIT 10;
+
+
+
+
+
+
 
 
